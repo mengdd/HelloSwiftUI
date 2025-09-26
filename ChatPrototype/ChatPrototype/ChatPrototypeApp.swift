68b32764cfa43e22a2e4eeeca44f299b1b4992ac
Dandan Meng <mengdandanno1@163.com>
2025-09-26 14:57:00 +0800

    create a new project

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Dandan Meng on 2025/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
